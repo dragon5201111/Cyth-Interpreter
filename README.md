fbc245e53f80df0a2ddd9ed9b5d8ec5b84c3f36e
evoexo <dragon520111back@gmail.com>
2026-01-27 18:04:17 -0500

    Added exit primitive function.

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 # Contents
 - [Build Instructions](#build-instructions)
 - [Documentation](#documentation)
+- [Usage](#usage)
 # Build Instructions
+# Usage
 # Documentation
--- include/Builtin.h
+++ include/Builtin.h
@@ -15,6 +15,14 @@ namespace Builtins {
                 std::cout << arg << " ";
             }
             return Value();
+        })},
+
+        {"exit", std::make_shared<PrimitiveFunction>([](const std::vector<Value>& args) {
+            if (args.size() != 1) {
+                throw std::invalid_argument("Exit function must have exactly 1 number argument.");
+            }
+            exit(static_cast<int>(args[0].as_number()));
+            return Value(); // Dummy return value
         })}
     };
 }
