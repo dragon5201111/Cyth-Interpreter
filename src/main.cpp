3071c51141c2e174dc24b0b3363d6eca9d20a93d
evoexo <dragon520111back@gmail.com>
2026-02-03 18:05:28 -0500

    Added usage.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,33 @@
+#include <getopt.h>
 #include "Interpreter.h"
 #include "Parser.h"
 #include "../include/Reader.h"
 #include "../include/Tokenizer.h"
-#include <getopt.h>
-
 #include "AstPrinter.h"
+
+void usage();
+void print_usage_option(const std::string& option, const std::string& description);
 
 int main(int argc, char ** argv) {
     bool print_ast = false;
     std::string source;
 
     constexpr option options[] = {
-        {"print-ast", 0, nullptr, 'a'}
+        {"print-ast", 0, nullptr, 'a'},
+        {"help", 0, nullptr, 'h'},
+        {nullptr, 0, nullptr, 0},
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "s:", options, nullptr)) != -1) {
+    while ((opt = getopt_long(argc, argv, "s:h", options, nullptr)) != -1) {
         if (opt == 's') {
             source = std::string(optarg);
         }else if (opt == 'a') {
             print_ast = true;
+        }else if (opt == '?' || opt == 'h') {
+            if (opt == '?') std::cout << "Unknown option: "<< argv[optind - 1] <<std::endl;
+            usage();
+            return 0;
         }
     }
 
@@ -40,4 +48,18 @@ int main(int argc, char ** argv) {
 
     program_decl->accept(interpreter);
     return 0;
+}
+
+void usage() {
+    std::cout << "Usage: Interpreter [options]" << std::endl;
+    std::cout << "Options:" << std::endl;
+    print_usage_option("-s", "Specify source file to execute.");
+    print_usage_option("--print-ast", "Print the ast of the source file to stdout.");
+}
+
+void print_usage_option(const std::string& option, const std::string& description){
+    std::cout.width(20);
+    std::cout.setf(std::ios::left);
+    std::cout << option;
+    std::cout << description << std::endl;
 }
