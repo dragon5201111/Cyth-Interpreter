a2b69190134613856d5d98bd0c6ad8edd1053f78
evoexo <dragon520111back@gmail.com>
2026-02-13 12:37:55 -0500

    Working on number class, to refactor value class.

---
+++ include/Num.h
@@ -0,0 +1,124 @@
+#pragma once
+#include <cstdint>
+#include <variant>
+
+#include "Print.h"
+
+class Number : public Printable{
+public:
+    std::variant<int64_t, double> value;
+
+    explicit Number(const int64_t value) : value(value) {}
+    explicit Number(const double value) : value(value) {}
+
+    operator int64_t() const { return std::get<int64_t>(value); }
+    operator double() const { return std::get<double>(value); }
+
+    [[nodiscard]] bool is_zero() const {
+        return std::visit([](auto&& value) {
+            return value == 0;
+        }, value);
+    }
+
+    Number operator*(const Number& other) const {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+            return Number(lhs * rhs);
+        }, value, other.value);
+    }
+
+    Number operator/(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+           return Number(lhs / rhs);
+       }, value, other.value);
+    }
+
+    Number operator-(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+           return Number(lhs - rhs);
+       }, value, other.value);
+    }
+
+    Number operator-() {
+        return std::visit([](auto&& value) {
+          return Number(-value);
+      }, value);
+    }
+
+    Number operator~() {
+        return std::visit([](auto&& value) {
+          return Number(~static_cast<int64_t>(value));
+      }, value);
+    }
+
+    Number operator%(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+                   return Number(static_cast<int64_t>(lhs) % static_cast<int64_t>(rhs));
+        }, value, other.value);
+    }
+
+    bool operator<(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+           return lhs < rhs;
+        }, value, other.value);
+    }
+
+    bool operator<=(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+           return lhs <= rhs;
+        }, value, other.value);
+    }
+
+    bool operator>(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+           return lhs > rhs;
+        }, value, other.value);
+    }
+
+    bool operator>=(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+           return lhs >= rhs;
+        }, value, other.value);
+    }
+
+    Number operator&(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+            return Number(static_cast<int64_t>(lhs) & static_cast<int64_t>(rhs));
+        }, value, other.value);
+    }
+
+    Number operator|(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+            return Number(static_cast<int64_t>(lhs) | static_cast<int64_t>(rhs));
+        }, value, other.value);
+    }
+
+    Number operator^(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+            return Number(static_cast<int64_t>(lhs) ^ static_cast<int64_t>(rhs));
+        }, value, other.value);
+    }
+
+    Number operator<<(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+            return Number(static_cast<int64_t>(lhs) << static_cast<int64_t>(rhs));
+        }, value, other.value);
+    }
+
+    Number operator>>(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+            return Number(static_cast<int64_t>(lhs) >> static_cast<int64_t>(rhs));
+        }, value, other.value);
+    }
+
+    Number operator+(const Number& other) {
+        return std::visit([](auto&& lhs, auto&& rhs) {
+           return Number(lhs + rhs);
+       }, value, other.value);
+    }
+
+    [[nodiscard]] std::string to_string() const override {
+        return std::visit([](auto&& value) {
+            return std::to_string(value);
+        }, value);
+    }
+};
