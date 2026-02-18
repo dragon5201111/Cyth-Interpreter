cc0c2200c43b04d624718d505673e64c40003620
Dawson De Mond <dragon520111@gmail.com>
2026-02-18 12:29:50 -0500

    Working on wrapper.

--- include/Container.h
+++ include/Container.h
@@ -3,39 +3,48 @@
 #include <utility>
 #include "Value.h"
 
-class Container : public Printable {
+class AbstractContainer : public Printable {
 protected:
     std::string left_closing;
     std::string right_closing;
 
-    Container(std::string left, std::string right)
+    AbstractContainer(std::string left, std::string right)
         : left_closing(std::move(left)),
           right_closing(std::move(right)) {}
 
 public:
-    ~Container() override = default;
-    [[nodiscard]] virtual bool is_empty() const = 0;
-    [[nodiscard]] virtual bool equals(const Container& other) const = 0;
-    [[nodiscard]] bool operator==(const Container& other) const {
-        return equals(other);
-    }
+    ~AbstractContainer() override = default;
+
+    [[nodiscard]] virtual bool empty() const = 0;
+    [[nodiscard]] virtual size_t size() const = 0;
+
+    [[nodiscard]] virtual Value& operator[](size_t index) = 0;
+    [[nodiscard]] virtual Value& operator[](Value index) = 0;
+
+    [[nodiscard]] virtual bool equals(const AbstractContainer& other) const = 0;
+    [[nodiscard]] bool operator==(const AbstractContainer& other) const { return equals(other); }
+
     [[nodiscard]] std::string to_string() const override = 0;
 };
 
 template<typename C>
-class BaseContainer : public Container {
+class BaseContainer : public AbstractContainer {
 protected:
     C container;
 public:
     BaseContainer(C c, std::string left, std::string right)
-        : Container(std::move(left), std::move(right)),
+        : AbstractContainer(std::move(left), std::move(right)),
           container(std::move(c)) {}
 
-    [[nodiscard]] bool is_empty() const override {
+    [[nodiscard]] bool empty() const override {
         return container.empty();
     }
 
-    [[nodiscard]] bool equals(const Container &other) const override {
+    [[nodiscard]] size_t size() const override {
+        return container.size();
+    }
+
+    [[nodiscard]] bool equals(const AbstractContainer &other) const override {
         if (typeid(*this) != typeid(other)) {
             return false;
         }
@@ -63,7 +72,7 @@ class ArrayContainer : public BaseContainer<std::deque<Value>> {
 public:
     ArrayContainer() : BaseContainer({}, "[", "]") {}
 
-    Value& operator[](const std::size_t index) {
+    Value& operator[](const std::size_t index) override {
         return container[index];
     }
 };
